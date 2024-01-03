21604dfa2cc348a9001568c1bb95e12346f2875d
MoltenWolfCub <oliver@tomlins.org.uk>
2024-01-03 10:59:49 +0000

    exported more of Object
    the fields and methods necessary for creating new objects are now
    exported so other packages can do so

--- worldObjects.go
+++ worldObjects.go
@@ -6,15 +6,15 @@ import (
 )
 
 type Object struct {
-	verticies    []float32 //in XYZ UV
-	vertexStride int       // 5 if using XYZ UV
+	Verticies    []float32 //in XYZ UV
+	VertexStride int       // 5 if using XYZ UV
 	normals      []float32
 	bufferLoader *BufferLoader
 	vao          BufferID
 	nao          BufferID
 }
 
-func (o *Object) fillBuffers() {
+func (o *Object) FillBuffers() {
 	o.bufferLoader = NewBufferLoader()
 	o.vao = GenBindVertexArray()
 	o.nao = GenBindBuffer(gl.ARRAY_BUFFER)
@@ -22,22 +22,22 @@ func (o *Object) fillBuffers() {
 	GenBindBuffer(gl.ARRAY_BUFFER) //VBO
 
 	BindVertexArray(o.vao)
-	o.bufferLoader.BuildFloatBuffer(o.vao, NewBufferLayout([]int32{3, 2}, o.verticies))
+	o.bufferLoader.BuildFloatBuffer(o.vao, NewBufferLayout([]int32{3, 2}, o.Verticies))
 	gl.BindBuffer(gl.ARRAY_BUFFER, uint32(o.nao))
 	o.bufferLoader.BuildFloatBuffer(o.nao, NewBufferLayout([]int32{3}, o.normals))
 }
 
-func (o *Object) calcNormals(triangleCount int) {
+func (o *Object) CalcNormals(triangleCount int) {
 	vertexCount := triangleCount * 3 //3 bc we are working in 3d space so XYZ
 
 	o.normals = make([]float32, vertexCount*3)
 	for tri := 0; tri < triangleCount; tri++ {
-		index := tri * o.vertexStride * 3
-		p1 := mgl32.Vec3{o.verticies[index], o.verticies[index+1], o.verticies[index+2]}
-		index += o.vertexStride
-		p2 := mgl32.Vec3{o.verticies[index], o.verticies[index+1], o.verticies[index+2]}
-		index += o.vertexStride
-		p3 := mgl32.Vec3{o.verticies[index], o.verticies[index+1], o.verticies[index+2]}
+		index := tri * o.VertexStride * 3
+		p1 := mgl32.Vec3{o.Verticies[index], o.Verticies[index+1], o.Verticies[index+2]}
+		index += o.VertexStride
+		p2 := mgl32.Vec3{o.Verticies[index], o.Verticies[index+1], o.Verticies[index+2]}
+		index += o.VertexStride
+		p3 := mgl32.Vec3{o.Verticies[index], o.Verticies[index+1], o.Verticies[index+2]}
 
 		normal := TriangleNormal(p1, p2, p3)
 		o.normals[tri*9+0] = normal.X()
@@ -58,7 +58,7 @@ func (o Object) Draw(shader Shader, drawMatrix mgl32.Mat4) {
 	BindVertexArray(o.vao)
 
 	shader.SetMatrix4("model", drawMatrix)
-	gl.DrawArrays(gl.TRIANGLES, 0, int32(len(o.verticies)/o.vertexStride))
+	gl.DrawArrays(gl.TRIANGLES, 0, int32(len(o.Verticies)/o.VertexStride))
 }
 
 func (o Object) DrawMultiple(shader Shader, num int, drawMatrix func(int) mgl32.Mat4) {
@@ -66,13 +66,13 @@ func (o Object) DrawMultiple(shader Shader, num int, drawMatrix func(int) mgl32.
 
 	for i := 0; i < num; i++ {
 		shader.SetMatrix4("model", drawMatrix(i))
-		gl.DrawArrays(gl.TRIANGLES, 0, int32(len(o.verticies)/o.vertexStride))
+		gl.DrawArrays(gl.TRIANGLES, 0, int32(len(o.Verticies)/o.VertexStride))
 	}
 }
 
 func Cube(size float32) Object {
 	o := Object{}
-	o.verticies = []float32{
+	o.Verticies = []float32{
 		-size / 2, -size / 2, -size / 2, 0.0, 0.0,
 		size / 2, size / 2, -size / 2, 1.0, 1.0,
 		size / 2, -size / 2, -size / 2, 1.0, 0.0,
@@ -115,17 +115,17 @@ func Cube(size float32) Object {
 		-size / 2, size / 2, -size / 2, 0.0, 1.0,
 		-size / 2, size / 2, size / 2, 0.0, 0.0,
 	}
-	o.vertexStride = 5
+	o.VertexStride = 5
 
-	o.calcNormals(12)
-	o.fillBuffers()
+	o.CalcNormals(12)
+	o.FillBuffers()
 
 	return o
 }
 
 func Pentahedron(size float32) Object {
 	o := Object{}
-	o.verticies = []float32{
+	o.Verticies = []float32{
 		size / 2, -size / 2, size / 2, 0.0, 1.0,
 		-size / 2, -size / 2, -size / 2, 1.0, 0.0,
 		size / 2, -size / 2, -size / 2, 0.0, 0.0,
@@ -149,10 +149,10 @@ func Pentahedron(size float32) Object {
 		-size / 2, -size / 2, -size / 2, 1.0, 0.0,
 		-size / 2, -size / 2, size / 2, 0.0, 0.0,
 	}
-	o.vertexStride = 5
+	o.VertexStride = 5
 
-	o.calcNormals(6)
-	o.fillBuffers()
+	o.CalcNormals(6)
+	o.FillBuffers()
 
 	return o
 }
